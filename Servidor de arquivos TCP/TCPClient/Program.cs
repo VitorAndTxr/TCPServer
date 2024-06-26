9a972e275b5f42e64a0895df279b9c1a41e80c90
Vitor Teixeira <vitor.teixeira@brainz.group>
2024-06-26 02:02:26 -0300

    aaaaaa

--- Servidor de arquivos TCP/TCPClient/Program.cs
+++ Servidor de arquivos TCP/TCPClient/Program.cs
@@ -3,120 +3,233 @@ using System.IO;
 using System.Net.Sockets;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading;
+using System.Threading.Tasks;
 
-class TCPClient
+class TCPClientService
 {
     private const string SERVER_IP = "127.0.0.1";
     private const int PORT = 12345;
 
-    static void Main(string[] args)
-    {
-        var client = new TcpClient(SERVER_IP, PORT);
-        var stream = client.GetStream();
-        var reader = new StreamReader(stream, Encoding.UTF8);
-        var writer = new StreamWriter(stream, Encoding.UTF8) { AutoFlush = true };
+    private static TcpClient _client;
+    private static NetworkStream _stream;
 
-        while (true)
-        {
-            var message = Console.ReadLine();
-
-            if(message!= null){
-                if (message.ToLower() == "sair")
-                {
-                    break;
-                }
-                else if (message.StartsWith("Arquivo"))
-                {
-                    writer.WriteLine(message);
-
-                    var fileName = message.Substring(7).Trim();
-                    ReceiveFile(fileName, writer, reader, stream);
-                }
-
-            }
-
-        }
-
-        client.Close();
-    }
-
-    private static void ListenForServerMessages(StreamReader reader, StreamWriter writer, NetworkStream stream)
+    static async Task Main(string[] args)
     {
         try
         {
-            while (true)
-            {
-                var message = reader.ReadLine();
-                if (message == null) break;
-                if(message.StartsWith("Arquivo")){
-                    var fileName = message.Substring(7).Trim();
-                    ReceiveFile(fileName, writer, reader, stream);
-                }
-                Console.WriteLine(message);
-            }
+            _client = new TcpClient();
+            await _client.ConnectAsync(SERVER_IP, PORT);
+
+            _stream = _client.GetStream();
+
+            Console.WriteLine("Conectado ao servidor.");
+
+            // Tarefas assíncronas para ouvir mensagens do servidor e enviar mensagens do console
+            var listenTask = ListenForServerMessagesAsync();
+            var sendTask = SendConsoleMessagesAsync();
+
+            // Aguardar que ambas as tarefas sejam concluídas
+            await Task.WhenAll(listenTask, sendTask);
         }
         catch (Exception ex)
         {
             Console.WriteLine($"Erro: {ex.Message}");
         }
+        finally
+        {
+            _client.Close();
+            Console.WriteLine("Conexão fechada.");
+        }
     }
 
-    private static void ReceiveFile(string fileName, StreamWriter writer, StreamReader reader, NetworkStream stream)
+    private static async Task SendConsoleMessagesAsync()
     {
-        var status = reader.ReadLine();
-        Console.WriteLine(status);
-
-
-        if (status == "Status: nok")
+        try
         {
-            status = reader.ReadLine();
-            Console.WriteLine(status);
-            return;
-        }
-        
-        var name = reader.ReadLine().Substring(6);
-        Console.WriteLine(name);
+            var writer = new StreamWriter(_stream, Encoding.UTF8) { AutoFlush = true };
 
-        var sizeString = reader.ReadLine().Substring(8);
-        Console.WriteLine(sizeString);
-
-        if (!long.TryParse(sizeString, out long size))
-        {
-            Console.WriteLine("Erro ao converter o tamanho do arquivo.");
-            return;
-        }
-        var hash = reader.ReadLine().Substring(6);
-
-        Console.WriteLine("Hash: " + hash);
-
-        var buffer = new byte[size];
-        using (var fileStream = new FileStream($"received_{name}", FileMode.Create, FileAccess.Write))
-        {
-            long totalBytesRead = 0;
-            while (totalBytesRead < size)
+            while (_client.Connected)
             {
-                int bytesRead = stream.Read(buffer, 0, buffer.Length);
-                fileStream.Write(buffer, 0, bytesRead);
-                totalBytesRead += bytesRead;
+                if (Console.KeyAvailable)
+                {
+                    var message = Console.ReadLine();
+                    Console.WriteLine($"{message}");
+
+                    if (message.ToLower() == "sair")
+                    {
+                        _client.Close();
+                        break;
+                    }
+                    if (message.StartsWith("Arquivo"))
+                    {
+                        writer.WriteLine(message);
+                    }
+                    else if (message.StartsWith("Chat"))
+                    {
+                        await SendMessageToServerAsync(message);
+                    }
+                }
+
+                await Task.Delay(100); // Pequena pausa para evitar loop de verificação intensivo
             }
         }
-
-        using (var sha256 = SHA256.Create())
+        catch (Exception ex)
         {
-            using (var fileStream = File.OpenRead($"received_{name}"))
+            Console.WriteLine($"Erro ao enviar mensagem: {ex.Message}");
+        }
+    }
+
+    private static async Task SendFileAsync(string fileName)
+    {
+        try
+        {
+            using (var fileStream = File.OpenRead(fileName))
             {
-                var computedHash = sha256.ComputeHash(fileStream);
-                var computedHashString = BitConverter.ToString(computedHash).Replace("-", "").ToLower();
-                if (computedHashString == hash)
+                var fileInfo = new FileInfo(fileName);
+                var hash = ComputeFileHash(fileStream);
+
+                StreamWriter writer = new StreamWriter(_stream, Encoding.UTF8) { AutoFlush = true };
+
+                // Enviar comando de arquivo
+                await writer.WriteLineAsync($"Arquivo {fileInfo.Name}");
+                await writer.WriteLineAsync($"Tamanho: {fileInfo.Length}");
+                await writer.WriteLineAsync($"Hash: {hash}");
+
+                // Enviar conteúdo do arquivo
+                await fileStream.CopyToAsync(_stream);
+                Console.WriteLine($"Arquivo {fileInfo.Name} enviado.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erro ao enviar arquivo: {ex.Message}");
+        }
+    }
+
+    private static async Task SendMessageToServerAsync(string message)
+    {
+        try
+        {
+            StreamWriter writer = new StreamWriter(_stream, Encoding.UTF8) { AutoFlush = true };
+            await writer.WriteLineAsync($"{message}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erro ao enviar mensagem para o servidor: {ex.Message}");
+        }
+    }
+
+    private static async Task ListenForServerMessagesAsync()
+    {
+        try
+        {
+            StreamReader reader = new StreamReader(_stream, Encoding.UTF8);
+
+            while (_client.Connected)
+            {
+                var teste = _stream.Socket.Available;
+                if (teste>0)
                 {
-                    Console.WriteLine("Arquivo recebido com sucesso e verificado.");
+
+                    var message = await reader.ReadLineAsync();
+
+                    if (message != null)
+                    {
+                        if (message.StartsWith("Mensagem do servidor:"))
+                        {
+                            Console.WriteLine(message);
+                        }
+                        else if (message.StartsWith("Arquivo"))
+                        {
+                            var fileName = message.Substring(7).Trim();
+                            await ReceiveFileAsync(fileName);
+                        }
+                    }
                 }
                 else
                 {
-                    Console.WriteLine("Erro de integridade no arquivo recebido.");
+                    await Task.Delay(100); // Pequena pausa para evitar loop de verificação intensivo
                 }
             }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erro na escuta de mensagens do servidor: {ex.Message}");
+        }
+    }
+
+    private static async Task ReceiveFileAsync(string fileName)
+    {
+        try
+        {
+            StreamReader reader = new StreamReader(_stream, Encoding.UTF8);
+
+            var status = await reader.ReadLineAsync();
+            Console.WriteLine(status);
+
+            if (status == "Status: nok")
+            {
+                status = await reader.ReadLineAsync();
+                Console.WriteLine(status);
+                return;
+            }
+
+            var name = (await reader.ReadLineAsync()).Substring(6);
+            Console.WriteLine(name);
+
+            var sizeString = (await reader.ReadLineAsync()).Substring(8);
+            Console.WriteLine(sizeString);
+
+            if (!long.TryParse(sizeString, out long size))
+            {
+                Console.WriteLine("Erro ao converter o tamanho do arquivo.");
+                return;
+            }
+
+            var hash = (await reader.ReadLineAsync()).Substring(6);
+            Console.WriteLine("Hash: " + hash);
+
+            var buffer = new byte[size];
+            using (var fileStream = new FileStream($"received_{fileName}", FileMode.Create, FileAccess.Write))
+            {
+                long totalBytesRead = 0;
+                while (totalBytesRead < size)
+                {
+                    int bytesRead = await _stream.ReadAsync(buffer, 0, buffer.Length);
+                    fileStream.Write(buffer, 0, bytesRead);
+                    totalBytesRead += bytesRead;
+                }
+            }
+
+            using (var sha256 = SHA256.Create())
+            {
+                using (var fileStream = File.OpenRead($"received_{name}"))
+                {
+                    var computedHash = sha256.ComputeHash(fileStream);
+                    var computedHashString = BitConverter.ToString(computedHash).Replace("-", "").ToLower();
+                    if (computedHashString == hash)
+                    {
+                        Console.WriteLine("Arquivo recebido com sucesso e verificado.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Erro de integridade no arquivo recebido.");
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erro ao receber arquivo: {ex.Message}");
+        }
+    }
+
+    private static string ComputeFileHash(FileStream fileStream)
+    {
+        using (var sha256 = SHA256.Create())
+        {
+            var hash = sha256.ComputeHash(fileStream);
+            return BitConverter.ToString(hash).Replace("-", "").ToLower();
         }
     }
 }
--- Servidor de arquivos TCP/TcpServer/Program.cs
+++ Servidor de arquivos TCP/TcpServer/Program.cs
@@ -1,107 +1,85 @@
-﻿using System;
-using System.IO;
-using System.Net;
+﻿using System.Net;
 using System.Net.Sockets;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading;
+
 
 class TCPServer
 {
-    private static TcpListener listener;
     private const int PORT = 12345;
+    public static int id = 0;
+    private static List<TCPClientHandler> clients = new List<TCPClientHandler>();
+    private static TcpListener listener = OpenSocket();
+    private static List<string> broadcastServerMessages = new List<string>();
 
     static void Main(string[] args)
+    {
+        MainLoop();
+    }
+
+    private static void MainLoop()
+    {
+        while (true)
+        {
+            FindNewClients();
+            SendBroadCast();
+        }
+    }
+
+    private static void SendBroadCast()
+    {
+        if(broadcastServerMessages.Count >0){
+
+            foreach(var client in clients){
+
+                lock(broadcastServerMessages){
+
+                    foreach(var broadcastServerMessage in broadcastServerMessages){
+
+                        client.SendBroadCast(broadcastServerMessage);
+                    }
+                }
+            }
+        }
+    }
+
+    private static void FindNewClients()
+    {
+        var client = listener?.AcceptTcpClient();
+
+        if (client != null)
+        {
+            if (client != null)
+            {
+
+                if(clients.Count == 0 ){
+                    id = 0;
+                }
+
+                id++;
+
+                var clientHandler = new TCPClientHandler(client, id);
+
+                var clientThread = new Thread(clientHandler.HandleClient);
+
+                clientThread.Start();
+
+                lock (clients)
+                {
+                    clients.Add(clientHandler);
+                }
+
+                Console.WriteLine($"Cliente {id} conectado.");
+
+            }
+        }
+    }
+
+    private static TcpListener OpenSocket()
     {
         listener = new TcpListener(IPAddress.Any, PORT);
         listener.Start();
         Console.WriteLine($"Servidor iniciado na porta {PORT}. Aguardando conexões...");
 
-        while (true)
-        {
-            var client = listener.AcceptTcpClient();
-            Console.WriteLine("Cliente conectado.");
-            var clientThread = new Thread(HandleClient);
-            clientThread.Start(client);
-        }
+        return listener;
     }
-
-    private static void HandleClient(object obj)
-    {
-        TcpClient client = (TcpClient)obj;
-        var stream = client.GetStream();
-        var reader = new StreamReader(stream, Encoding.UTF8);
-        var writer = new StreamWriter(stream, Encoding.UTF8) { AutoFlush = true };
-
-        try
-        {
-            while (true)
-            {
-                var message = reader.ReadLine();
-                if (message == null) break;
-
-                if (message.ToLower() == "sair")
-                {
-                    Console.WriteLine("Cliente desconectado.");
-                    break;
-                }
-                else if (message.StartsWith("Arquivo"))
-                {
-                    var fileName = message.Substring(7).Trim();
-                    SendFile(fileName, writer, stream);
-                }
-                else if (message.StartsWith("Chat"))
-                {
-                    var chatMessage = message.Substring(4).Trim();
-                    Console.WriteLine($"Cliente: {chatMessage}");
-                    writer.WriteLine($"Servidor: {chatMessage}");
-                }
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Erro: {ex.Message}");
-        }
-        finally
-        {
-            client.Close();
-        }
-    }
-
-    private static void SendFile(string fileName, StreamWriter writer, NetworkStream stream)
-    {
-        if (!File.Exists(fileName))
-        {
-            writer.WriteLine("Status: nok");
-            writer.WriteLine("Arquivo inexistente.");
-            return;
-        }
-
-
-        writer.WriteLine("Status: ok");
-        Console.WriteLine("Status: ok");
-
-        var fileInfo = new FileInfo(fileName);
-        writer.WriteLine($"Nome: {fileInfo.Name}");
-        Console.WriteLine($"Nome: {fileInfo.Name}");
-        
-        writer.WriteLine($"Tamanho: {fileInfo.Length}");
-        Console.WriteLine($"Tamanho: {fileInfo.Length}");
-
-
-        using (var sha256 = SHA256.Create())
-        {
-            using (var fileStream = File.OpenRead(fileName))
-            {
-                var hash = sha256.ComputeHash(fileStream);
-                var hashString = BitConverter.ToString(hash).Replace("-", "").ToLower();
-                writer.WriteLine($"Hash: {hashString}");
-            }
-        }
-
-        using (var fileStream = File.OpenRead(fileName))
-        {
-            fileStream.CopyTo(stream);
-        }
-    }
+ 
 }
